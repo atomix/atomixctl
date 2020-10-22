780c83aaa995735da69aee71684dde09ee3b2d36
Jordan Halterman <jordan.halterman@gmail.com>
2020-10-22 11:33:32 -0700

    Update value commands to new format

--- pkg/cli/command/value.go
+++ pkg/cli/command/value.go
@@ -16,119 +16,143 @@ package command
 
 import (
 	"context"
+	"fmt"
 	"github.com/atomix/go-client/pkg/client/value"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func newValueCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "value {get,set,watch}",
-		Short: "Manage the state of a distributed value",
+	return &cobra.Command{
+		Use:                "value <name> [...]",
+		Short:              "Manage the state of a distributed value",
+		Args:               cobra.MinimumNArgs(1),
+		DisableFlagParsing: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// If only the name was specified, open an interactive shell
+			name := args[0]
+			if len(args) == 1 {
+				return runShell(fmt.Sprintf("value:%s", args[0]), os.Stdin, os.Stdout, os.Stderr, append(os.Args[1:], "value", name))
+			}
+
+			// Get the command for the specified operation
+			var subCmd *cobra.Command
+			op := args[1]
+			switch op {
+			case "set":
+				subCmd = newValueSetCommand(name)
+			case "get":
+				subCmd = newValueGetCommand(name)
+			case "watch":
+				subCmd = newValueWatchCommand(name)
+			}
+
+			// Set the arguments after the name and execute the command
+			subCmd.SetArgs(args[2:])
+			return subCmd.Execute()
+		},
 	}
-	addClientFlags(cmd)
-	cmd.PersistentFlags().StringP("name", "n", "", "the value name")
-	cmd.PersistentFlags().Lookup("name").Annotations = map[string][]string{
-		cobra.BashCompCustom: {"__atomix_get_values"},
-	}
-	cmd.MarkPersistentFlagRequired("name")
-	cmd.AddCommand(newValueSetCommand())
-	cmd.AddCommand(newValueGetCommand())
-	cmd.AddCommand(newValueWatchCommand())
-	return cmd
 }
 
-func getValueName(cmd *cobra.Command) string {
-	name, _ := cmd.Flags().GetString("name")
-	return name
-}
-
-func getValue(cmd *cobra.Command, name string) value.Value {
+func getValue(cmd *cobra.Command, name string) (value.Value, error) {
 	database := getDatabase(cmd)
 	ctx, cancel := getTimeoutContext(cmd)
 	defer cancel()
-	m, err := database.GetValue(ctx, name)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	}
-	return m
+	return database.GetValue(ctx, name)
 }
 
-func newValueSetCommand() *cobra.Command {
+func newValueSetCommand(name string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "set <value>",
 		Args: cobra.ExactArgs(1),
-		Run:  runValueSetCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			val, err := getValue(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			version, _ := cmd.Flags().GetUint64("version")
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			var newVersion uint64
+			if version > 0 {
+				newVersion, err = val.Set(ctx, []byte(args[0]), value.IfVersion(version))
+			} else {
+				newVersion, err = val.Set(ctx, []byte(args[0]))
+			}
+
+			if err != nil {
+				return err
+			}
+			cmd.Println(newVersion)
+			return nil
+		},
 	}
 	cmd.Flags().Uint64("version", 0, "the value version to update (for optimistic locking)")
 	return cmd
 }
 
-func runValueSetCommand(cmd *cobra.Command, args []string) {
-	val := getValue(cmd, getValueName(cmd))
-	version, _ := cmd.Flags().GetUint64("version")
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	var newVersion uint64
-	var err error
-	if version > 0 {
-		newVersion, err = val.Set(ctx, []byte(args[0]), value.IfVersion(version))
-	} else {
-		newVersion, err = val.Set(ctx, []byte(args[0]))
-	}
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithOutput("Value: %s, Version: %d", args[0], newVersion)
-	}
-}
-
-func newValueGetCommand() *cobra.Command {
+func newValueGetCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "get",
 		Args: cobra.NoArgs,
-		Run:  runValueGetCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			val, err := getValue(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			version, _ := cmd.Flags().GetUint64("version")
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			value, version, err := val.Get(ctx)
+			if err != nil {
+				return err
+			}
+
+			bytes, err := yaml.Marshal(map[string]interface{}{"value": value, "version": version})
+			if err != nil {
+				return err
+			}
+			cmd.Println(string(bytes))
+			return nil
+		},
 	}
 }
 
-func runValueGetCommand(cmd *cobra.Command, _ []string) {
-	val := getValue(cmd, getValueName(cmd))
-	version, _ := cmd.Flags().GetUint64("version")
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	value, version, err := val.Get(ctx)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithOutput("Value: %s, Version: %d", string(value), version)
-	}
-}
-
-func newValueWatchCommand() *cobra.Command {
+func newValueWatchCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "watch",
 		Args: cobra.NoArgs,
-		Run:  runValueWatchCommand,
-	}
-}
+		RunE: func(cmd *cobra.Command, args []string) error {
+			val, err := getValue(cmd, name)
+			if err != nil {
+				return err
+			}
 
-func runValueWatchCommand(cmd *cobra.Command, _ []string) {
-	val := getValue(cmd, getValueName(cmd))
-	watchCh := make(chan *value.Event)
-	if err := val.Watch(context.Background(), watchCh); err != nil {
-		ExitWithError(ExitError, err)
-	}
+			watchCh := make(chan *value.Event)
+			if err := val.Watch(context.Background(), watchCh); err != nil {
+				return err
+			}
 
-	signalCh := make(chan os.Signal, 2)
-	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-	for {
-		select {
-		case event := <-watchCh:
-			Output("Value: %s, Version: %d", string(event.Value), event.Version)
-		case <-signalCh:
-			ExitWithSuccess()
-		}
+			signalCh := make(chan os.Signal, 2)
+			signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+			for {
+				select {
+				case event := <-watchCh:
+					bytes, err := yaml.Marshal(event)
+					if err != nil {
+						cmd.Println(err)
+					} else {
+						cmd.Println(string(bytes))
+					}
+				case <-signalCh:
+					return nil
+				}
+			}
+		},
 	}
 }
