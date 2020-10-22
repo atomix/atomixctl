79d9b686b83a7193e510da033a4e6de5661f6a91
Jordan Halterman <jordan.halterman@gmail.com>
2020-10-22 11:23:38 -0700

    Update log commands to new format

--- pkg/cli/command/log.go
+++ pkg/cli/command/log.go
@@ -17,8 +17,10 @@ package command
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/atomix/go-client/pkg/client/log"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 	"os"
 	"os/signal"
 	"strconv"
@@ -26,208 +28,231 @@ import (
 )
 
 func newLogCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "log {get,append,remove,entries,size,clear,watch}",
-		Short: "Manage the state of a distributed log",
+	return &cobra.Command{
+		Use:                "log <name> [...]",
+		Short:              "Manage the state of a distributed log",
+		Args:               cobra.MinimumNArgs(1),
+		DisableFlagParsing: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// If only the name was specified, open an interactive shell
+			name := args[0]
+			if len(args) == 1 {
+				return runShell(fmt.Sprintf("counter:%s", args[0]), os.Stdin, os.Stdout, os.Stderr, append(os.Args[1:], "log", name))
+			}
+
+			// Get the command for the specified operation
+			var subCmd *cobra.Command
+			op := args[1]
+			switch op {
+			case "get":
+				subCmd = newLogGetCommand(name)
+			case "append":
+				subCmd = newLogAppendCommand(name)
+			case "remove":
+				subCmd = newLogRemoveCommand(name)
+			case "entries":
+				subCmd = newLogEntriesCommand(name)
+			case "size":
+				subCmd = newLogSizeCommand(name)
+			case "clear":
+				subCmd = newLogClearCommand(name)
+			case "watch":
+				subCmd = newLogWatchCommand(name)
+			}
+
+			// Set the arguments after the name and execute the command
+			subCmd.SetArgs(args[2:])
+			return subCmd.Execute()
+		},
 	}
-	addClientFlags(cmd)
-	cmd.PersistentFlags().StringP("name", "n", "", "the log name")
-	cmd.PersistentFlags().Lookup("name").Annotations = map[string][]string{
-		cobra.BashCompCustom: {"__atomix_get_logs"},
-	}
-	cmd.MarkPersistentFlagRequired("name")
-	cmd.AddCommand(newLogGetCommand())
-	cmd.AddCommand(newLogAppendCommand())
-	cmd.AddCommand(newLogRemoveCommand())
-	cmd.AddCommand(newLogEntriesCommand())
-	cmd.AddCommand(newLogSizeCommand())
-	cmd.AddCommand(newLogClearCommand())
-	cmd.AddCommand(newLogWatchCommand())
-	return cmd
 }
 
-func getLogName(cmd *cobra.Command) string {
-	name, _ := cmd.Flags().GetString("name")
-	return name
-}
-
-func getLog(cmd *cobra.Command, name string) log.Log {
+func getLog(cmd *cobra.Command, name string) (log.Log, error) {
 	database := getDatabase(cmd)
 	ctx, cancel := getTimeoutContext(cmd)
 	defer cancel()
-	l, err := database.GetLog(ctx, name)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	}
-	return l
+	return database.GetLog(ctx, name)
 }
 
-func newLogGetCommand() *cobra.Command {
+func newLogGetCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "get <index>",
 		Args: cobra.ExactArgs(1),
-		Run:  runLogGetCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			indexStr := args[0]
+			index, err := strconv.Atoi(indexStr)
+			if err != nil {
+				return err
+			}
+
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			entry, err := l.Get(ctx, log.Index(index))
+			if err != nil {
+				return err
+			} else if entry != nil {
+				bytes, err := yaml.Marshal(entry)
+				if err != nil {
+					return err
+				}
+				cmd.Println(string(bytes))
+			}
+			return nil
+		},
 	}
 }
 
-func runLogGetCommand(cmd *cobra.Command, args []string) {
-	l := getLog(cmd, getLogName(cmd))
-	indexStr := args[0]
-	index, err := strconv.Atoi(indexStr)
-	if err != nil {
-		ExitWithError(ExitBadArgs, err)
-	}
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	entry, err := l.Get(ctx, log.Index(index))
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else if entry != nil {
-		ExitWithOutput("Index: %d, Value: %v", entry.Index, entry.Value)
-	} else {
-		ExitWithOutput("<none>")
-	}
-}
-
-func newLogAppendCommand() *cobra.Command {
+func newLogAppendCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "append <value>",
 		Args: cobra.ExactArgs(1),
-		Run:  runLogAppendCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			value := args[0]
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			entry, err := l.Append(ctx, []byte(value))
+			if err != nil {
+				return err
+			} else if entry != nil {
+				bytes, err := yaml.Marshal(entry)
+				if err != nil {
+					return err
+				}
+				cmd.Println(string(bytes))
+			}
+			return nil
+		},
 	}
 }
 
-func runLogAppendCommand(cmd *cobra.Command, args []string) {
-	l := getLog(cmd, getLogName(cmd))
-	value := args[0]
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	entry, err := l.Append(ctx, []byte(value))
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithOutput("Index: %d, Value: %v", entry.Index, entry.Value)
-	}
-}
-
-func newLogRemoveCommand() *cobra.Command {
+func newLogRemoveCommand(name string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "remove <index>",
 		Args: cobra.ExactArgs(1),
-		Run:  runLogRemoveCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			indexStr := args[0]
+			index, err := strconv.Atoi(indexStr)
+			if err != nil {
+				return err
+			}
+
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			entry, err := l.Remove(ctx, log.Index(index))
+			if err != nil {
+				return err
+			} else if entry != nil {
+				bytes, err := yaml.Marshal(entry)
+				if err != nil {
+					return err
+				}
+				cmd.Println(string(bytes))
+			}
+			return nil
+		},
 	}
 	cmd.Flags().Int64P("version", "v", 0, "the entry version")
 	return cmd
 }
 
-func runLogRemoveCommand(cmd *cobra.Command, args []string) {
-	m := getLog(cmd, getLogName(cmd))
-	indexStr := args[0]
-	index, err := strconv.Atoi(indexStr)
-	if err != nil {
-		ExitWithError(ExitBadArgs, err)
-	}
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	entry, err := m.Remove(ctx, log.Index(index))
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else if entry != nil {
-		ExitWithOutput("Index: %d, Value: %v", entry.Index, entry.Value)
-	} else {
-		ExitWithOutput("<none>")
-	}
-}
-
-func newLogEntriesCommand() *cobra.Command {
+func newLogEntriesCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "entries",
 		Args: cobra.NoArgs,
-		Run:  runLogEntriesCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return errors.New("not implemented")
+		},
 	}
 }
 
-func runLogEntriesCommand(cmd *cobra.Command, _ []string) {
-	ExitWithError(ExitBadFeature, errors.New("not implemented"))
-}
-
-func newLogSizeCommand() *cobra.Command {
+func newLogSizeCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "size",
 		Args: cobra.NoArgs,
-		Run:  runLogSizeCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			size, err := log.Size(ctx)
+			if err != nil {
+				return err
+			}
+			cmd.Println(size)
+			return nil
+		},
 	}
 }
 
-func runLogSizeCommand(cmd *cobra.Command, _ []string) {
-	log := getLog(cmd, getLogName(cmd))
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	size, err := log.Size(ctx)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithOutput("%d", size)
-	}
-}
-
-func newLogClearCommand() *cobra.Command {
+func newLogClearCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "clear",
 		Args: cobra.NoArgs,
-		Run:  runLogClearCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			return log.Clear(ctx)
+		},
 	}
 }
 
-func runLogClearCommand(cmd *cobra.Command, _ []string) {
-	log := getLog(cmd, getLogName(cmd))
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	err := log.Clear(ctx)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithSuccess()
-	}
-}
-
-func newLogWatchCommand() *cobra.Command {
+func newLogWatchCommand(name string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "watch",
 		Args: cobra.NoArgs,
-		Run:  runLogWatchCommand,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			l, err := getLog(cmd, name)
+			if err != nil {
+				return err
+			}
+			watchCh := make(chan *log.Event)
+			opts := []log.WatchOption{}
+			replay, _ := cmd.Flags().GetBool("replay")
+			if replay {
+				opts = append(opts, log.WithReplay())
+			}
+			if err := l.Watch(context.Background(), watchCh, opts...); err != nil {
+				ExitWithError(ExitError, err)
+			}
+
+			signalCh := make(chan os.Signal, 2)
+			signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+			for {
+				select {
+				case event := <-watchCh:
+					bytes, err := yaml.Marshal(event)
+					if err != nil {
+						return err
+					}
+					cmd.Println(string(bytes))
+				case <-signalCh:
+					return nil
+				}
+			}
+		},
 	}
 	cmd.Flags().BoolP("replay", "r", false, "replay current log entries at start")
 	return cmd
-}
-
-func runLogWatchCommand(cmd *cobra.Command, _ []string) {
-	l := getLog(cmd, getLogName(cmd))
-	watchCh := make(chan *log.Event)
-	opts := []log.WatchOption{}
-	replay, _ := cmd.Flags().GetBool("replay")
-	if replay {
-		opts = append(opts, log.WithReplay())
-	}
-	if err := l.Watch(context.Background(), watchCh, opts...); err != nil {
-		ExitWithError(ExitError, err)
-	}
-
-	signalCh := make(chan os.Signal, 2)
-	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-	for {
-		select {
-		case event := <-watchCh:
-			switch event.Type {
-			case log.EventNone:
-				Output("Replayed: Index: %d, Value: %v", event.Entry.Index, event.Entry.Value)
-			case log.EventAppended:
-				Output("Appended: Index: %d, Value: %v", event.Entry.Index, event.Entry.Value)
-			case log.EventRemoved:
-				Output("Removed: Index: %d, Value: %v", event.Entry.Index, event.Entry.Value)
-			}
-		case <-signalCh:
-			ExitWithSuccess()
-		}
-	}
 }
