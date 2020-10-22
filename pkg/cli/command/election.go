5c6470f8bf92b393ad60ebbdf94070bd36551a2c
Jordan Halterman <jordan.halterman@gmail.com>
2020-10-22 02:59:55 -0700

    Update election commands to new format

--- pkg/cli/command/election.go
+++ pkg/cli/command/election.go
@@ -16,137 +16,183 @@ package command
 
 import (
 	"context"
+	"fmt"
 	"github.com/atomix/go-client/pkg/client/election"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func newElectionCommand() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "election {enter,get,watch}",
-		Short: "Managed the state of a distributed leader election",
+	return &cobra.Command{
+		Use:                "election <name> [...]",
+		Short:              "Manage the state of a distributed leader election",
+		Args:               cobra.MinimumNArgs(1),
+		DisableFlagParsing: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// If only the name was specified, open an interactive shell
+			name := args[0]
+			if len(args) == 1 {
+				return runShell(fmt.Sprintf("election:%s", args[0]), os.Stdin, os.Stdout, os.Stderr, append(os.Args[1:], "election", name))
+			}
+
+			// Get the command for the specified operation
+			var subCmd *cobra.Command
+			op := args[1]
+			switch op {
+			case "enter":
+				subCmd = newElectionEnterCommand(name)
+			case "get":
+				subCmd = newElectionGetCommand(name)
+			case "watch":
+				subCmd = newElectionWatchCommand(name)
+			}
+
+			// Set the arguments after the name and execute the command
+			subCmd.SetArgs(args[2:])
+			return subCmd.Execute()
+		},
 	}
-	addClientFlags(cmd)
-	cmd.PersistentFlags().StringP("name", "n", "", "the election name")
-	cmd.PersistentFlags().Lookup("name").Annotations = map[string][]string{
-		cobra.BashCompCustom: {"__atomix_get_elections"},
-	}
-	cmd.MarkPersistentFlagRequired("name")
-	cmd.AddCommand(newElectionGetCommand())
-	cmd.AddCommand(newElectionEnterCommand())
-	cmd.AddCommand(newElectionWatchCommand())
-	return cmd
 }
 
-func getElectionName(cmd *cobra.Command) string {
-	name, _ := cmd.Flags().GetString("name")
-	return name
-}
-
-func getElection(cmd *cobra.Command, name string, id string) election.Election {
+func getElection(cmd *cobra.Command, name string, id string) (election.Election, error) {
 	database := getDatabase(cmd)
 	ctx, cancel := getTimeoutContext(cmd)
 	defer cancel()
-	m, err := database.GetElection(ctx, name, election.WithID(id))
-	if err != nil {
-		ExitWithError(ExitError, err)
-	}
-	return m
+	return database.GetElection(ctx, name, election.WithID(id))
 }
 
-func newElectionGetCommand() *cobra.Command {
+func newElectionGetCommand(name string) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "get {leader,term}",
+	}
+	cmd.AddCommand(newElectionGetLeaderCommand(name))
+	cmd.AddCommand(newElectionGetTermCommand(name))
+	return cmd
+}
+
+func newElectionGetLeaderCommand(name string) *cobra.Command {
 	return &cobra.Command{
-		Use:  "get",
-		Args: cobra.NoArgs,
-		Run:  runElectionGetCommand,
+		Use: "leader [options]",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			election, err := getElection(cmd, name, "")
+			if err != nil {
+				return err
+			}
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			term, err := election.GetTerm(ctx)
+			if err != nil {
+				return err
+			} else if term != nil {
+				cmd.Println(term.Leader)
+			}
+			return nil
+		},
 	}
 }
 
-func runElectionGetCommand(cmd *cobra.Command, _ []string) {
-	election := getElection(cmd, getElectionName(cmd), "")
-	ctx, cancel := getTimeoutContext(cmd)
-	defer cancel()
-	term, err := election.GetTerm(ctx)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	} else {
-		ExitWithOutput("Term: %d, Leader: %s, Candidates: %v", term.ID, term.Leader, term.Candidates)
+func newElectionGetTermCommand(name string) *cobra.Command {
+	return &cobra.Command{
+		Use: "term [options]",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			election, err := getElection(cmd, name, "")
+			if err != nil {
+				return err
+			}
+			ctx, cancel := getTimeoutContext(cmd)
+			defer cancel()
+			term, err := election.GetTerm(ctx)
+			if err != nil {
+				return err
+			} else if term != nil {
+				bytes, err := yaml.Marshal(term)
+				if err != nil {
+					return err
+				}
+				cmd.Println(string(bytes))
+			}
+			return nil
+		},
 	}
 }
 
-func newElectionEnterCommand() *cobra.Command {
+func newElectionEnterCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "enter <id>",
 		Args: cobra.ExactArgs(1),
-		Run:  runElectionEnterCommand,
-	}
-}
-
-func runElectionEnterCommand(cmd *cobra.Command, args []string) {
-	watchCh := make(chan *election.Event)
-	election := getElection(cmd, getElectionName(cmd), args[0])
-	ctx, cancel := getTimeoutContext(cmd)
-
-	// Create a watch on the election
-	err := election.Watch(context.Background(), watchCh)
-	if err != nil {
-		ExitWithError(ExitError, err)
-	}
-
-	// Enter the election
-	_, err = election.Enter(ctx)
-	cancel()
-	if err != nil {
-		ExitWithError(ExitError, err)
-	}
-
-	// Once we've successfully entered the election, wait for watch events
-	Output("Entered election")
-	signalCh := make(chan os.Signal, 2)
-	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-	for {
-		select {
-		case event := <-watchCh:
-			Output("Election state changed; Term: %d, Leader: %s, Candidates %v", event.Term.ID, event.Term.Leader, event.Term.Candidates)
-		case <-signalCh:
-			Output("Leaving election")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			watchCh := make(chan *election.Event)
+			election, err := getElection(cmd, name, args[0])
+			if err != nil {
+				return err
+			}
 			ctx, cancel := getTimeoutContext(cmd)
-			_, err = election.Leave(ctx)
+
+			// Create a watch on the election
+			err = election.Watch(context.Background(), watchCh)
+			if err != nil {
+				ExitWithError(ExitError, err)
+			}
+
+			// Enter the election
+			_, err = election.Enter(ctx)
 			cancel()
 			if err != nil {
 				ExitWithError(ExitError, err)
-			} else {
-				ExitWithSuccess()
 			}
-		}
+
+			// Once we've successfully entered the election, wait for watch events
+			Output("Entered election")
+			signalCh := make(chan os.Signal, 2)
+			signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+			for {
+				select {
+				case event := <-watchCh:
+					Output("Election state changed; Term: %d, Leader: %s, Candidates %v", event.Term.ID, event.Term.Leader, event.Term.Candidates)
+				case <-signalCh:
+					Output("Leaving election")
+					ctx, cancel := getTimeoutContext(cmd)
+					_, err = election.Leave(ctx)
+					cancel()
+					if err != nil {
+						ExitWithError(ExitError, err)
+					} else {
+						ExitWithSuccess()
+					}
+				}
+			}
+		},
 	}
 }
 
-func newElectionWatchCommand() *cobra.Command {
+func newElectionWatchCommand(name string) *cobra.Command {
 	return &cobra.Command{
 		Use:  "watch",
 		Args: cobra.NoArgs,
-		Run:  runElectionWatchCommand,
-	}
-}
+		RunE: func(cmd *cobra.Command, args []string) error {
+			e, err := getElection(cmd, name, "")
+			if err != nil {
+				return err
+			}
 
-func runElectionWatchCommand(cmd *cobra.Command, _ []string) {
-	e := getElection(cmd, getElectionName(cmd), "")
-	watchCh := make(chan *election.Event)
-	if err := e.Watch(context.Background(), watchCh); err != nil {
-		ExitWithError(ExitError, err)
-	}
+			watchCh := make(chan *election.Event)
+			if err := e.Watch(context.Background(), watchCh); err != nil {
+				ExitWithError(ExitError, err)
+			}
 
-	signalCh := make(chan os.Signal, 2)
-	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-	for {
-		select {
-		case event := <-watchCh:
-			Output("Term: %d, Leader: %s, Candidates: %v", event.Term.ID, event.Term.Leader, event.Term.Candidates)
-		case <-signalCh:
-			ExitWithSuccess()
-		}
+			signalCh := make(chan os.Signal, 2)
+			signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+			for {
+				select {
+				case event := <-watchCh:
+					Output("Term: %d, Leader: %s, Candidates: %v", event.Term.ID, event.Term.Leader, event.Term.Candidates)
+				case <-signalCh:
+					ExitWithSuccess()
+				}
+			}
+		},
 	}
 }
